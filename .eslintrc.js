2717673fe95e4ca6e3ed458b48cccd63ee9bca77
yuu04rip <99676204+yuu04rip@users.noreply.github.com>
2025-07-31 01:05:32 +0200

    Update

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    env: {
+        node: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    rules: {
+        // Aggiungi qui le tue regole personalizzate, se necessario
+    },
+};
