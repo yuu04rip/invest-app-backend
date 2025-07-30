cb941ca9b2fbc4c0274538d382626b74fa5112d9
yuu04rip <99676204+yuu04rip@users.noreply.github.com>
2025-07-31 00:40:45 +0200

    Aggioramenti

--- src/routes/product.js
+++ src/routes/product.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 const auth = require('../middleware/auth');
 const validate = require('../middleware/validate');
+const apiLimiter = require('../middleware/rateLimiter');
 const {
     createProductValidator,
     updateProductValidator,
@@ -10,10 +11,10 @@ const {
 } = require('../middleware/productValidators');
 
 // CRUD prodotti
-router.post('/', auth, createProductValidator, validate, productController.createProduct);
+router.post('/', auth, apiLimiter,createProductValidator, validate, productController.createProduct);
 router.get('/', productController.getAllProducts);
 router.get('/:id', idParamValidator, validate, productController.getProductById);
-router.put('/:id', auth, idParamValidator, validate, updateProductValidator, validate, productController.updateProductById);
-router.delete('/:id', auth, idParamValidator, validate, productController.deleteProductById);
+router.put('/:id', auth,apiLimiter, idParamValidator, validate, updateProductValidator, validate, productController.updateProductById);
+router.delete('/:id', auth, apiLimiter, idParamValidator, validate, productController.deleteProductById);
 
 module.exports = router;
